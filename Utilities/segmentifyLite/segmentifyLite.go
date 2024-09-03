c9f4f2099cd4ef15b72293750b04d39b1485eb4c
Jason Vicinanza <116429288+flaneur7508@users.noreply.github.com>
2024-09-03 14:16:54 +0100

    3/9

--- Utilities/segmentifyLite/segmentifyLite.go
+++ Utilities/segmentifyLite/segmentifyLite.go
@@ -517,8 +517,10 @@ func segmentFolders(thresholdValue int, slashCount int, folderLevel string) {
 		}
 
 		//Split the line into substrings using a forward-slash as delimiter
-		// slashCount = 4 for Level 1 folders
-		// slashCount = 5 for Level 2 folders
+		// slashCount
+		// = 4 for Level 1 folders
+		// slashCount
+		// = 5 for Level 2 folders
 		parts := strings.Split(line, "/")
 
 		if len(parts) >= slashCount {
